8e9d604466e7fcb17c8f9c53f4347a53677f6d8d
FIRST_NAME LAST_NAME <ciencias.computacion.games@gmail.com>
2021-04-08 14:51:34 -0400

    Refactoring game logic into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
 use iui::prelude::*;
 use iui::controls::{Spacer, Label, Button, VerticalBox,HorizontalBox,LayoutGrid,GridExpand,GridAlignment};
-use std::rc::Rc;
-use std::cell::RefCell;
 
 use std::io;
 use io::stdin;
 
-// #[derive(Debug)]
-// enum GameError{
+use std::fs;
+use std::fs::File;
 
-//     InvalidBoardPositionError
-// }
-
-// enum BoardPosition
-// {
-//     valid, invalid,
-// }
-
-struct State 
-{
-    label:String,
-}
+// use TicTacToe::{ButtonGrid, Game};
 
 #[derive(Clone)]
 struct ButtonGrid{
@@ -49,7 +36,6 @@ impl Game
 
     fn set_player_board_idx(&mut self, index: i8)-> bool
     {
-        //we need to check if valid index
         if self.clone().check_board_position(index) &&
            self.board[index as usize] == ""
         {
@@ -81,7 +67,7 @@ impl Game
 
     fn current_player_token(&self)-> String
     {
-        self.players[self.current_turn() as usize % 2].clone()
+        self.players[self.current_player()-1].clone()
     }
 
     fn current_board_state(&self)-> [String;9]
@@ -142,6 +128,18 @@ impl Game
         }
 
         return false
+    }
+
+    fn write_current_board(&self)-> std::io::Result<()>
+    {
+        // let mut file = File::create("");
+
+        for pos in self.current_board_state().iter()
+        {
+            fs::write("game.txt",  pos.to_string())?;
+        }
+
+        Ok(())
     }
 
     fn is_winning(&self)-> bool
@@ -254,7 +252,7 @@ fn build_button_grid(ui:&UI)->ButtonGrid
 }
 
 
-fn build_game(players:[String; 2])->Game{
+fn build_game(players: [String; 2])->Game{
 
     Game{
         board: [
@@ -300,9 +298,9 @@ fn cli_main() -> io::Result<()>
                 Ok(_ ) => match input.trim().parse::<i8>()
                 {
                     Ok(n) => n,
-                    Err(_) => 11//invalid board indexes
+                    Err(_) => 11//invalid board indices
                 },
-                Err(_) => 11//invalid board indexes
+                Err(_) => 11
             };
             
             if game.set_player_board_idx(parsed_index)
@@ -328,22 +326,8 @@ fn cli_main() -> io::Result<()>
 
 }
 
-fn main()
+fn create_layout_grid(ui:&UI, game:& mut Game, button_grid:&mut ButtonGrid, label:& Label)-> LayoutGrid
 {
-    //init game board
-    let players:[String; 2] = [String::from("X"),String::from("O")];
-    let mut game = Box::new(build_game(players));
-
-    // Initialize the UI and State
-    let ui = UI::init().expect("Couldn't initialize UI library");
-    let state = Rc::new(RefCell::new(State { label: "".into() }));
-
-    //Buttons
-    let mut button_grid = build_button_grid(&ui);
-
-    // Create a window into which controls can be placed
-    let mut win = Window::new(&ui, "Tic Tac Toe", 200, 100, WindowType::NoMenubar);
-
     // Create a vertical layout to hold the controls
     let mut layout_grid = LayoutGrid::new(&ui);
     let mut vbox = VerticalBox::new(&ui);
@@ -360,9 +344,9 @@ fn main()
     vbox.set_padded(&ui, true);
 
     // Create a new label.
-    let label = Label::new(&ui, "");
+    // let label = Label::new(&ui, "");
 
-    // Create a button and its callback
+    // Create buttons and their callbacks
     let mut i:i8 = 0;
 
     while i < button_grid.buttons.len() as i8
@@ -408,35 +392,121 @@ fn main()
 
     layout_grid.append(&ui, vbox.clone(), 50 ,60, 5,5,GridExpand::Both,GridAlignment::Start,GridAlignment::Center);
 
-    win.set_child(&ui, layout_grid);
+    layout_grid
+}
+
+fn main()
+{
+    //init game board
+    let players:[String; 2] = [String::from("X"),String::from("O")];
+    let mut game = Box::new(build_game(players));
+
+    // Initialize the UI and State
+    let ui = UI::init().expect("Couldn't initialize UI library");
+    // let label = Label::new(&ui, "");
+    //Buttons
+    let mut button_grid = build_button_grid(&ui);
+
+    // Create a window into which controls can be placed
+    let mut win = Window::new(&ui, "Tic Tac Toe", 200, 100, WindowType::NoMenubar);
+
+    //SETUP UI Elements
+
+    // Create a vertical layout to hold the controls
+    let mut layout_grid = LayoutGrid::new(&ui);
+    let mut vbox = VerticalBox::new(&ui);
+    let mut hbox_0 = HorizontalBox::new(&ui);
+    let mut hbox_1 = HorizontalBox::new(&ui);
+    let mut hbox_2 = HorizontalBox::new(&ui);
+
+    hbox_0.set_padded(&ui, true);
+    hbox_1.set_padded(&ui, true);
+    hbox_2.set_padded(&ui, true);
+
+
+    layout_grid.set_padded(&ui, true);
+    vbox.set_padded(&ui, true);
+
+    // Create a new label.
+    let label = Label::new(&ui, "");
+
+    // Create buttons and its callback
+    let mut i:i8 = 0;
+
+    while i < button_grid.buttons.len() as i8
+    {
+        button_grid.buttons[i as usize].on_clicked(&ui, {
+            | button: &mut Button  | {
+                if button.text(&ui) != "X" && button.text(&ui) != "O"
+                {
+                    button.set_text( &ui, String::from(game.current_player_token()).as_str());
+                    game.update_turn();
+                }
+            }
+        });
+
+        i+=1;
+    }
+
+    // THIS IS TERRIBLE CODING BUT IDK HOW ELSE TO DO IT
+    // TODO: Refactor this into ECS Architecture!
+    for k in 0..3{
+        hbox_0.append(&ui,button_grid.buttons[k].clone(), LayoutStrategy::Compact);
+    }
+
+    for k in 3..6{
+        hbox_1.append(&ui,button_grid.buttons[k].clone(), LayoutStrategy::Compact);
+    }
+
+    for k in 6..9{
+        hbox_2.append(&ui,button_grid.buttons[k].clone(), LayoutStrategy::Compact);
+    }
+    
+    // Adding controls to the box, and box to window
+    vbox.append(&ui, Spacer::new(&ui), LayoutStrategy::Stretchy);
+    vbox.append(&ui, Spacer::new(&ui), LayoutStrategy::Stretchy);
+    vbox.append(&ui, label.clone(), LayoutStrategy::Stretchy);
+    
+
+    vbox.append(&ui, hbox_0.clone(), LayoutStrategy::Stretchy);
+    vbox.append(&ui, hbox_1.clone(), LayoutStrategy::Stretchy);
+    vbox.append(&ui, hbox_2.clone(), LayoutStrategy::Stretchy);
+
+    layout_grid.append(&ui, Spacer::new(&ui), 0 ,10, 5,5,GridExpand::Both,GridAlignment::Start,GridAlignment::Center);
+
+    layout_grid.append(&ui, vbox.clone(), 50 ,60, 5,5,GridExpand::Both,GridAlignment::Start,GridAlignment::Center);
+    // let layout_grid = create_layout_grid(&ui, & mut game,& mut button_grid, &label);
+    win.set_child(&ui, layout_grid );
     // Show the window
     win.show(&ui);
 
     // Run the application
     let mut event_loop = ui.event_loop();
 
+    // The on tick event loop is overkill
+    // TODO: Make this loop use only event listeners and not a clock tic
+    // Too many calls here for events that are IO dependent.
     event_loop.on_tick(&ui, {
 
         let ui = ui.clone();
         let mut ui_label = label.clone();
 
-        // println!("Event Loop Turn: #{}", game.current_turn());
-        
-        if game.current_turn()  >= game.max_turns() && check_button_states(&button_grid, &ui)
+        if game.current_turn()  >= game.max_turns() && !check_button_states(&button_grid.clone(), &ui.clone())
         {
             println!("No Winner Possible!\n\n");
             println!("...Game Over...");
+
+            ui_label.set_text(&ui, "Draw!");
+            // TODO: Implement Reset functionality and ask players to play again
         }
     
         // End of turn
         move || {
             // Update all the labels
-    // ui_label.set_text(&ui, &format!("Turn #{}", game.current_turn()));
-
-            if check_button_states(&button_grid, &ui)
+            if check_button_states(&button_grid.clone(), &ui.clone())
             {
                 ui_label.set_text(&ui, format!("Player {} Won!",
-                                  game.current_player()).as_str());
+                                  game.current_player()-1).as_str());
                 // reset(& mut button_grid, &ui);
             }
         }
